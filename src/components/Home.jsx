e3f29fc861b10d74f68ef4174869d29b4ec6aa26
shuyanglin <shuyanglin.tw@gmail.com>
2024-10-06 11:31:52 +0100

    fetch dummy data and incorperated in upcoming activity

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "../assets/css/Home.css";
 import { Button } from "@headlessui/react";
 import {
@@ -39,6 +40,18 @@ import { SunIcon } from "@heroicons/react/solid";
 // ];
 
 export default function Home() {
+  const [events, setEvents] = useState([]);
+
+  // do useEffect here
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setEvents(data);
+      });
+  }, []);
+
   return (
     <>
       <section className="home-header">
@@ -170,14 +183,17 @@ export default function Home() {
           <div className="upcoming-activity">
             <h1>Upcoming activity</h1>
             <div className="scroll-container">
-              <div className="card">
-                <div className="card-image">
-                  <img src="https://s3-alpha-sig.figma.com/img/39b3/d91c/f4ee7fa94c87ba6c92f862f6af225046?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oM5qrPhbOnpzMlJqkeDO51PemRGABFwsjZ4NQMb8vksHDe8taf33ewAviJMwas32e9PYqjxLGJFkYHu9ntGb6VmsgQ1ADHjChfpGAzTt-qh1t0JrMZdM3oGu~JdNX0GyiSwboWHmHObkLTFcpAReLlMqJLDjsdUUk-XXMJUUYXVPyMZ3tnMa6VrZ1qOhMSCbQTij-i6bYsW07CmrNn-8fusT1Mi2-2DjGlzGi~2bBrOZfPZwjhMzgv-LngjDdZeIUNCMEWKd6ASlmOyE~cq7eL-swkZIVr-FN98rM5eJrh4JXNZoIUgo0L9SWSIz3z3ZG-m1qkGPPL63zc2ZNb2Akg__"></img>
+              {" "}
+              {events.map((event) => (
+                <div className="card" key={event.id}>
+                  <div className="card-image">
+                    <img src="https://s3-alpha-sig.figma.com/img/39b3/d91c/f4ee7fa94c87ba6c92f862f6af225046?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oM5qrPhbOnpzMlJqkeDO51PemRGABFwsjZ4NQMb8vksHDe8taf33ewAviJMwas32e9PYqjxLGJFkYHu9ntGb6VmsgQ1ADHjChfpGAzTt-qh1t0JrMZdM3oGu~JdNX0GyiSwboWHmHObkLTFcpAReLlMqJLDjsdUUk-XXMJUUYXVPyMZ3tnMa6VrZ1qOhMSCbQTij-i6bYsW07CmrNn-8fusT1Mi2-2DjGlzGi~2bBrOZfPZwjhMzgv-LngjDdZeIUNCMEWKd6ASlmOyE~cq7eL-swkZIVr-FN98rM5eJrh4JXNZoIUgo0L9SWSIz3z3ZG-m1qkGPPL63zc2ZNb2Akg__"></img>
+                  </div>
+                  <div className="card-title">{event.name}</div>
+                  <div className="card-date">{event.address.city}</div>
                 </div>
-                <div className="card-title">Cooking class</div>
-                <div className="card-date">2024.8.12</div>
-              </div>
-              <div className="card">
+              ))}
+              {/* <div className="card">
                 <div className="card-image">
                   <img src="https://s3-alpha-sig.figma.com/img/d098/ee3b/6c2b37f58d15cb93e78d348bbe7c1aa6?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SetPzCP-2gX7uU-~J86pgcDzm51dT3LGGBXCbyoTn8Cfp2d69pZps3oKbX~LSP19EgKuumlXeHf2cp5KM-2aL~gQqQDxhdtJa5BZjR-6H0UrXBurpr9NRWJLhFiu8M1Y39CbGpDXt7MsTPakYEhFb7Y8W8F3tQlZz6WSuUhGlwGnS0Zv-wZLrbCCMFMFqM4wXxBOE6AqzJ4c4eC-MnvutY5JoLAzZjeZyeRpVpcuzBhXLzCcYgMEwjcA4jKxB2BOYvpDjROLEXBTSZ2oQ6E~XCz941YfwciAlMVzgUk6XZHla45uuumXNQ20jAK2cOmqiHwCSDLJgJnUy33CTOMFgw__"></img>
                 </div>
@@ -218,7 +234,7 @@ export default function Home() {
                 </div>
                 <div className="card-title">Cooking class</div>
                 <div className="card-date">2024.8.12</div>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="past-activity">

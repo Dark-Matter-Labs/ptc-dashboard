7d1a761394d6ff5cf0f08a5f4d65dec8d542b19f
corner_coder <56599360+fatihohn@users.noreply.github.com>
2024-11-19 19:49:56 +0900

    src/lib/PermissionEngine/api: add type declaration

--- src/lib/PermissionEngine/api/index.js
+++ src/lib/PermissionEngine/api/index.js
@@ -10,6 +10,44 @@ export class API {
    */
 
   /**
+   * @typedef {Object} CreateRuleBlockBody
+   * @property {string} name - RuleBlock name.
+   * @property {string} type - RuleBlock type.
+   * @property {string} content - RuleBlock content.
+   * @property {string?} details - RuleBlock details.
+   * @property {string?} file - RuleBlock file.
+   */
+
+  /**
+   * @typedef {Object} CreateRuleBody
+   * @property {string} name - Rule name.
+   * @property {string?} parentRuleId - Rule parentRuleId.
+   * @property {string} target - Rule target.
+   * @property {string[]} ruleBlockIds - Rule ruleBlockIds.
+   * @property {string[]?} topicIds - Rule topicIds.
+   */
+
+  /**
+   * @typedef {Object} CreateSpaceEventBody
+   * @property {string} name - Event name.
+   * @property {string?} spaceId - Event spaceId.
+   * @property {string?} ruleId - Event ruleId.
+   * @property {string?} externalServiceId - Event externalServiceId.
+   * @property {string?} details - Event details.
+   * @property {string?} link - Event link.
+   * @property {string?} callbackLink - Event callbackLink.
+   * @property {string} duration - Event duration.
+   * @property {string} startsAt - Event startsAt.
+   * @property {string[]?} images - Event images.
+   * @property {string[]?} topicIds - Event topicIds.
+   */
+
+  /**
+   * @typedef {Object} CreateEventPermissionRequestBody
+   * @property {string} spaceEventId - PermissionRequest spaceEventId.
+   */
+
+  /**
    * Constructs a query string from an object.
    * @param {Object} params - The query parameters as key-value pairs.
    * @returns {string} The query string.
@@ -163,8 +201,8 @@ export class API {
     return response?.data ?? [];
   };
 
-  fetchSpaceApprovedRulesByRelevance = async (spaceId, selectedTopics) => {
-    if (!selectedTopics || selectedTopics.length === 0) {
+  fetchSpaceApprovedRulesByRelevance = async (spaceId, topicIds) => {
+    if (!topicIds || topicIds.length === 0) {
       console.log("No selected topics, return empty array");
       return [];
     }
@@ -174,7 +212,7 @@ export class API {
       {},
       {
         spaceId,
-        topicIds: selectedTopics.map((item) => item.id).join(","),
+        topicIds: topicIds.join(","),
       }
     );
 
@@ -187,16 +225,10 @@ export class API {
     return response?.data ?? [];
   };
 
-  fetchSpaceRuleBlocksBySpaceRuleId = async (spaceRuleId) => {
-    const response = await this.get("rule/:spaceRuleId", { spaceRuleId });
+  fetchRuleBlocksByRuleId = async (ruleId) => {
+    const response = await this.get("rule/:ruleId", { ruleId });
 
-    return response?.data?.ruleBlocks ?? [];
-  };
-
-  fetchEventRuleBlocksByEventRuleId = async (eventRuleId) => {
-    const response = await this.get("rule/:eventRuleId", { eventRuleId });
-
-    return response?.data?.ruleBlocks ?? [];
+    return response?.ruleBlocks ?? [];
   };
 
   fetchSpaceApprovedRulesSortBy = async (spaceId, sortBy) => {
@@ -227,10 +259,17 @@ export class API {
   };
 
   // Fetch avaiabilities of a space
-  fetchAvailability = async (spaceId) => {
-    const response = await this.get("space/:spaceId/availability", {
-      spaceId,
-    });
+  fetchAvailability = async (spaceId, startDate = null, endDate = null) => {
+    const response = await this.get(
+      "space/:spaceId/availability",
+      {
+        spaceId,
+      },
+      {
+        startDate,
+        endDate,
+      }
+    );
 
     return response?.data ?? [];
   };
@@ -304,5 +343,131 @@ export class API {
     });
 
     return response;
+  };
+
+  /**
+   *
+   * @param {CreateRuleBlockBody} body
+   * @returns RuleBlock
+   */
+  createRuleBlock = async (body) => {
+    const { name, type, content, details, file } = body;
+    const formData = new FormData();
+
+    if (type) {
+      formData.append("type", type);
+    }
+
+    if (name) {
+      formData.append("name", name);
+    }
+
+    if (content) {
+      formData.append("content", content);
+    }
+
+    if (details) {
+      formData.append("details", details);
+    }
+
+    if (file) {
+      formData.append("files", [file]);
+    }
+
+    const response = await this.post("rule/block", {}, formData);
+
+    return response;
+  };
+
+  /**
+   *
+   * @param {CreateRuleBody} body
+   * @returns
+   */
+  createRule = async (body) => {
+    const response = await this.post("rule", {}, body);
+
+    return response;
+  };
+
+  /**
+   *
+   * @param {CreateSpaceEventBody} body
+   * @returns
+   */
+  createSpaceEvent = async (body) => {
+    const {
+      name,
+      spaceId,
+      ruleId,
+      externalServiceId,
+      details,
+      link,
+      callbackLink,
+      duration,
+      startsAt,
+      images,
+      topicIds,
+    } = body;
+    const formData = new FormData();
+
+    if (name) {
+      formData.append("name", name);
+    }
+
+    if (spaceId) {
+      formData.append("spaceId", spaceId);
+    }
+
+    if (ruleId) {
+      formData.append("ruleId", ruleId);
+    }
+
+    if (externalServiceId) {
+      formData.append("externalServiceId", externalServiceId);
+    }
+
+    if (details) {
+      formData.append("details", details);
+    }
+
+    if (link) {
+      formData.append("link", link);
+    }
+
+    if (callbackLink) {
+      formData.append("callbackLink", callbackLink);
+    }
+
+    if (duration) {
+      formData.append("duration", duration);
+    }
+
+    if (startsAt) {
+      formData.append("startsAt", startsAt);
+    }
+
+    if (images) {
+      formData.append("images", images);
+    }
+
+    if (topicIds) {
+      formData.append("topicIds", topicIds);
+    }
+
+    const response = await this.post("event", {}, body);
+
+    return response;
+  };
+
+  /**
+   *
+   * @param {CreateEventPermissionRequestBody} body
+   * @returns
+   */
+  createEventPermissionRequest = async (body) => {
+    const response = await this.post("permission/request/event", {}, body);
+
+    return response?.data?.permissionRequest;
   };
 }

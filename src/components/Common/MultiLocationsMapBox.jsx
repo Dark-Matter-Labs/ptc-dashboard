80aa861379b4ddb1794081a88b7e3390ed9746be
shuyanglin <shuyanglin.tw@gmail.com>
2024-11-26 12:49:33 +0900

    multilocation

---
+++ src/components/Common/MultiLocationsMapBox.jsx
@@ -0,0 +1,64 @@
+import { useState, useRef, useEffect } from "react";
+import Map, { Marker } from "react-map-gl";
+import mapboxgl from "mapbox-gl";
+import PropTypes from "prop-types";
+import { accessToken } from "../../lib/mapbox";
+// import { LocationMarkerIcon } from "@heroicons/react/solid";
+import MapPinGreenImgSrc from "../../assets/image/map-pin-green.svg";
+
+mapboxgl.accessToken = accessToken;
+
+export const MultiLocationsMapBox = ({
+  option,
+  locations,
+  currentLanguage,
+}) => {
+  const mapRef = useRef();
+  const defaultStyle = "mapbox://styles/mapbox/streets-v11";
+  //defaultLocation in Daegu
+  const defaultLocation = {
+    latitude: 35.8709,
+    longitude: 128.6021,
+  };
+  const [viewport] = useState({
+    width: option?.width ?? "100%",
+    height: option?.height ?? 400,
+    initialViewState: {
+      zoom: option?.zoom ?? 15,
+      latitude: Number(locations[0]?.latitude ?? defaultLocation.latitude),
+      longitude: Number(locations[0]?.longitude ?? defaultLocation.longitude),
+    },
+    mapStyle:
+      currentLanguage === "en"
+        ? defaultStyle
+        : `${defaultStyle}?language=${currentLanguage}`,
+    language: currentLanguage,
+  });
+
+  useEffect(() => {
+    mapRef.current?.setLanguage(currentLanguage);
+  }, [currentLanguage]);
+
+  return (
+    <Map ref={mapRef} {...viewport} mapboxAccessToken={mapboxgl.accessToken}>
+      {/* Add a Marker */}
+      {locations.map((loc, index) => (
+        <Marker
+          key={index}
+          latitude={Number(loc?.latitude ?? defaultLocation.latitude)}
+          longitude={Number(loc?.longitude ?? defaultLocation.longitude)}
+        >
+          {/* <LocationMarkerIcon className="h-12 w-12 white mr-1 text-red-600" /> */}
+          <img src={MapPinGreenImgSrc} className="h-7 w-7" alt="map pin" />
+        </Marker>
+      ))}
+    </Map>
+  );
+};
+
+MultiLocationsMapBox.propTypes = {
+  locations: PropTypes.arrayOf(PropTypes.object),
+  option: PropTypes.object,
+  handleChangeLanguage: PropTypes.func,
+  currentLanguage: PropTypes.string,
+};
--- src/pages/Landing/LandingMap.jsx
+++ src/pages/Landing/LandingMap.jsx
@@ -1,5 +1,29 @@
 import PropTypes from "prop-types";
+
+import { MultiLocationsMapBox } from "../../components/Common/MultiLocationsMapBox";
 export const LandingMap = ({ mapSectionRef }) => {
+  // receive an array of topic id (family, friends, hobby, common)
+  // const topicIds = [
+  //   "009bdd05-6a8c-4b8e-9b50-f19583c7b0c2",
+  //   "97459921-e485-40b8-8787-3b8e9ab6c32c",
+  //   "ab246c90-e6be-4f46-8cc6-2f840d945ffc",
+  //   "17f8122d-e62e-4e96-9720-dca6fe59893d",
+  // ];
+
+  // filtered locations in Seoul
+
+  // filtered locations in Daegu
+  const locations = [
+    { latitude: 35.8709, longitude: 128.6021 },
+    { latitude: 35.8718, longitude: 128.6005 },
+    { latitude: 35.8721, longitude: 128.5997 },
+    { latitude: 35.8702, longitude: 128.6018 },
+    { latitude: 35.8713, longitude: 128.6026 },
+    { latitude: 35.872, longitude: 128.6009 },
+    { latitude: 35.8707, longitude: 128.601 },
+  ];
+
+  const currentLanguage = "en";
   return (
     <section
       ref={mapSectionRef}
@@ -17,6 +41,10 @@ export const LandingMap = ({ mapSectionRef }) => {
           선택한 키워드입니다.
         </div>
       </div>
+      <MultiLocationsMapBox
+        locations={locations}
+        currentLanguage={currentLanguage}
+      />
     </section>
   );
 };

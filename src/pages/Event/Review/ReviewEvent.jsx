e3aa2ff769ff01849c142ae13e2b7d1cc612c95c
shuyanglin <shuyanglin.tw@gmail.com>
2024-12-04 20:17:06 +0000

    reveal topics

--- src/pages/Event/Review/ReviewEvent.jsx
+++ src/pages/Event/Review/ReviewEvent.jsx
@@ -2,23 +2,12 @@ import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useUser } from "../../../useUser";
 import PropTypes from "prop-types";
+
 const ReviewEvent = ({ permissionEngineAPI }) => {
   const { user } = useUser();
   const { t } = useTranslation();
-  const [eventData, setEventData] = useState({
-    name: "Test Event Name",
-    // ruleId: null,
-    // duration: null, // to be collected from form
-    // startsAt: null, // to be collected from form
-    // externalServiceId: null,
-    // details: null,
-    // link: null,
-    // callbackLink: null,
-    // images: [],
-    // topicIds: [],
-    // privateRuleBlocks: [],
-    // requestType: null,
-  });
+  const [topics, setTopics] = useState([]);
+  const [eventData, setEventData] = useState({});
 
   const fetchEventById = async () => {
     // TODO. add pagination or infinite scroll feature
@@ -33,9 +22,27 @@ const ReviewEvent = ({ permissionEngineAPI }) => {
       });
   };
 
+  const interpretTopics = async () => {
+    try {
+      console.log("topics: ", eventData.topics);
+      const topicPromises = eventData?.topics?.map((item) =>
+        permissionEngineAPI.fetchTopicById(item.id)
+      );
+      const topicData = await Promise.all(topicPromises);
+      const topicNames = topicData?.map((topic) => topic.name);
+      setTopics(topicNames);
+    } catch (error) {
+      console.error("Error fetching topics: ", error);
+    }
+  };
+
   useEffect(() => {
     fetchEventById();
   }, []);
+
+  useEffect(() => {
+    interpretTopics();
+  }, [eventData]);
 
   console.log("user: ", user);
   return (
@@ -43,18 +50,45 @@ const ReviewEvent = ({ permissionEngineAPI }) => {
       {user ? (
         <div className="p-4 space-y-4 text-left">
           <div className="text-2xl block mb-2 font-semibold mt-8">
-            Review Event
+            Event Proposal
           </div>
           {/* Title */}
           <div className="py-4">
-            {/* <p className="mb-4">{t("create-event.event-title")}</p> */}
+            <p className="mb-2">{t("review-event.event-title")}</p>
             <div className="text-2xl font-semibold">{eventData.name}</div>
           </div>
           <hr />
+          {/* Theme */}
+          <div className="py-4">
+            <p className="mb-2">{t("review-event.event-theme")}</p>
+            <div className="text-2xl font-semibold">
+              {topics?.length > 0
+                ? topics?.map((topic, index) => (
+                    <div key={index} value={topic}>
+                      <div className=" cursor-pointer px-4 py-2 rounded-full text-sm font-medium  bg-white text-gray-800 border border-gray-400 w-fit ">
+                        {topic}
+                      </div>
+                    </div>
+                  ))
+                : "Themes not selected"}
+            </div>
+          </div>
+          {/* Date and Time */}
+          <div className="py-4">
+            <p className="mb-2">{t("review-event.event-date-time")}</p>
+            <div className="text-2xl font-semibold">{eventData.name}</div>
+          </div>
+          <hr />
+          {/* Description */}
+          <div className="py-4">
+            <p className="mb-2">{t("review-event.event-description")}</p>
+            <div className="text-2xl font-semibold">{eventData.details}</div>
+          </div>
+          <hr />
           {/* Organizer name and email */}
-          <p className="mb-4">{t("create-event.organizer-name")}</p>
+          <p className="mb-2">{t("review-event.organizer-name")}</p>
           <p className="text-xl font-semibold">{user.name}</p>
-          <p className="mb-4">{t("create-event.organizer-email")}</p>
+          <p className="mb-2">{t("review-event.organizer-email")}</p>
           <p className="text-xl font-semibold">{user.email}</p>
           <hr className="my-6"></hr>
         </div>

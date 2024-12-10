aa3ba69bd6c9adb96b5ca5b3ad9c9d0980a96efd
shuyanglin <shuyanglin.tw@gmail.com>
2024-12-10 11:43:26 +0000

    complicated drawer logic for filling in excitement and worries. the state decision saves agree, disagree or abstention, and only when excitement and worries are both filled would the value remains, otherwise they will all be rest and require user for further input. the scenarios include when user drag the drawer down drawer, clock back or save button without filling in excitment or worries propoerly.

--- src/pages/Event/Review/ReviewRulesWithExceptions.jsx
+++ src/pages/Event/Review/ReviewRulesWithExceptions.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { CheckCircleIcon } from "@heroicons/react/solid";
 import DisplayRulesWithExceptions from "../../Rule/DisplayRulesWithExceptions";
 import BottomDrawerReview from "../../../components/Common/BottomDrawerReview";
 import { useState, useEffect } from "react";
@@ -11,10 +12,25 @@ const ReviewRulesWithExceptions = ({
   console.log("rule with exeptions: ", rule);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [decision, setDecision] = useState(""); //agree, disagree, abstention
-  const handleButtonClicked = (decision) => {
-    if (decision === "agree") setDecision("agree");
-    if (decision === "disagree") setDecision("disagree");
-    if (decision === "abstention") setDecision("abstention");
+  const [excitements, setExcitements] = useState("");
+  const [worries, setWorries] = useState("");
+  const handleButtonClicked = (d) => {
+    //reset excitements and worries when another button is clicked
+    if (decision != "" && d !== decision) {
+      setExcitements("");
+      setWorries("");
+    }
+    //set decision accordingly to button clicked
+    if (d === "agree") {
+      setDecision("agree");
+    }
+    if (d === "disagree") {
+      setDecision("disagree");
+    }
+    if (d === "abstention") {
+      setDecision("abstention");
+    }
+
     setIsDrawerOpen(true);
   };
 
@@ -22,16 +38,21 @@ const ReviewRulesWithExceptions = ({
     console.log(isDrawerOpen);
   }, [isDrawerOpen]);
   return (
-    <div className="p-4 space-y-4 text-left">
-      <div className="text-2xl block mb-2 font-semibold mt-8">
-        {t("review-event.review-the-exceptions")}
+    <div className="flex flex-col justify-start p-4 space-y-2 text-left h-[90vh]">
+      <div className="flex-grow">
+        <div className="text-2xl block mb-2 font-semibold mt-8">
+          {t("review-event.review-the-exceptions")}
+          <p>decision: {decision}</p>
+          <p>excitements: {excitements}</p>
+          <p>worries: {worries}</p>
+        </div>
+        <DisplayRulesWithExceptions
+          rule={rule}
+          permissionEngineAPI={permissionEngineAPI}
+          proceedToStep={proceedToStep}
+        ></DisplayRulesWithExceptions>
       </div>
-      <DisplayRulesWithExceptions
-        rule={rule}
-        permissionEngineAPI={permissionEngineAPI}
-        proceedToStep={proceedToStep}
-      ></DisplayRulesWithExceptions>
-      <div className="py-4">
+      <div className="py-8">
         <button
           onClick={() => proceedToStep(2)}
           className="mt-4 px-6 py-2 border text-black rounded-lg w-full"
@@ -43,23 +64,44 @@ const ReviewRulesWithExceptions = ({
           onClick={() => handleButtonClicked("agree")}
         >
           {t("review-event.review-agree")}
+          {decision === "agree" && excitements && worries ? (
+            <CheckCircleIcon className="ml-2 h-6 w-6 inline" />
+          ) : (
+            ""
+          )}
         </button>
         <button
           className="mt-4 px-6 py-2 bg-[#FFF6F3] text-[#F47051] border border-1  border-[#F47051] hover:bg-[#F47051] hover:text-white rounded-full w-full"
           onClick={() => handleButtonClicked("disagree")}
         >
           {t("review-event.review-disagree")}
+          {decision === "disagree" && excitements && worries ? (
+            <CheckCircleIcon className="ml-2 h-6 w-6 inline" />
+          ) : (
+            ""
+          )}
         </button>
         <button
           className="mt-4 px-6 py-2 bg-[#F5F5F7] text-[#92929D] border border-1  border-[#92929D] hover:bg-[#92929D] hover:text-white rounded-full w-full"
           onClick={() => handleButtonClicked("abstention")}
         >
           {t("review-event.review-abstention")}
+          {decision === "abstention" && excitements && worries ? (
+            <CheckCircleIcon className="ml-2 h-6 w-6 inline" />
+          ) : (
+            ""
+          )}
         </button>
         {isDrawerOpen ? (
           <BottomDrawerReview
             setIsDrawerOpen={setIsDrawerOpen}
             decision={decision}
+            proceedToStep={proceedToStep}
+            excitements={excitements}
+            worries={worries}
+            setDecision={setDecision}
+            setWorries={setWorries}
+            setExcitements={setExcitements}
           />
         ) : (
           ""
